6924ef0e5321948baa28bae219dd48945aa204b8
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-07-22 22:56:11 +0300

    fix Card

--- src/components/ProductProperties.tsx
+++ src/components/ProductProperties.tsx
@@ -9,12 +9,10 @@ export const ProductProperties = (props: Props) => {
     <ul className="flex flex-col gap-y-2 my-2">
       {properties.map((property) => (
         <li key={property.name} className="flex justify-between">
-          <div className="text-xs text-grey_secondary font-bold">
+          <div className="text-xs text-secondary font-bold">
             {property.name}
           </div>
-          <div className="text-xs text-grey_primary font-bold">
-            {property.value}
-          </div>
+          <div className="text-xs text-primary font-bold">{property.value}</div>
         </li>
       ))}
     </ul>
