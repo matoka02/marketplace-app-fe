ba1fe27b3d66f01238e949251213996b738f71af
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-07-22 22:45:34 +0300

    add pagination:
    - images;
    - Card;
    - Pagination.

---
+++ src/components/ProductProperties.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  properties: { name: string; value: string }[];
+};
+
+export const ProductProperties = (props: Props) => {
+  const { properties } = props;
+
+  return (
+    <ul className="flex flex-col gap-y-2 my-2">
+      {properties.map((property) => (
+        <li key={property.name} className="flex justify-between">
+          <div className="text-xs text-grey_secondary font-bold">
+            {property.name}
+          </div>
+          <div className="text-xs text-grey_primary font-bold">
+            {property.value}
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
