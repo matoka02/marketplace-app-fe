e8de14cfbb628d86573d28abd297e2d9f3ed4915
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-07-24 14:13:56 +0300

    create components

---
+++ src/components/Line.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface LineProps {
+  width: string;
+  height?: string;
+}
+
+const Line: React.FC<LineProps> = ({ width, height = '1px' }) => {
+  return (
+    <span
+      style={{
+        height,
+      }}
+      className={`bg-elements inline-block ${width}`}
+    ></span>
+  );
+};
+
+export default Line;
