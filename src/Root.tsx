4ab3b5801207cdb43ad6055a49ef088533648caf
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-07-24 14:14:11 +0300

    update pages and routes

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,6 +5,8 @@ import NotFound from './pages/NotFound';
 import ScrollToTop from './components/ScrollToTop';
 import CatalogPage from './pages/CatalogPage';
 import CartPage from './pages/CartPage';
+import ProductPage from './pages/ProductPage';
+import FavoritesPage from './pages/FavoritesPage';
 
 export const Root = () => {
   <BrowserRouter>
@@ -14,7 +16,7 @@ export const Root = () => {
         <Route index element={<h1>Home</h1>} />
         <Route path="home" element={<Navigate to="/" replace />} />
         <Route path="phones" element={<CatalogPage />}>
-          <Route path="/:phoneId?" element={<h1>Phones Page</h1>} />
+          <Route path="/:phoneId?" element={<ProductPage />} />
         </Route>
         <Route path="tablets">
           <Route path="/:tabletId?" element={<h1>Tablets Page</h1>} />
@@ -22,11 +24,10 @@ export const Root = () => {
         <Route path="accessories">
           <Route path="/:accessoryId?" element={<h1>Accessories Page</h1>} />
         </Route>
+        <Route path="favorites" element={<FavoritesPage />} />
         <Route path="cart" element={<CartPage />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   </BrowserRouter>;
 };
-
-export default Root;
