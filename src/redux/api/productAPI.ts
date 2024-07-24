8bc09c649c42b02dae09d6da9aadbf6d8a90d58c
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-07-24 13:36:44 +0300

    add api-redux

---
+++ src/redux/api/productAPI.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+export const productApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://dreamteam.onrender.com',
+  }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: ({ page, perPage, sortBy }) => ({
+        url: `/products`,
+        params: { page, perPage, sortBy },
+      }),
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productApi;
