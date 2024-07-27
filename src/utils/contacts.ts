5cfb4fe1e80205cfa2a839de21c62eee8bccd0c2
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-07-27 18:44:49 +0300

    add contacts

---
+++ src/utils/contacts.ts
@@ -0,0 +1,14 @@
+import { Contact } from '../types/Contact';
+
+export const contacts: Contact[] = [
+  {
+    name: 'Ghost Matoka',
+    image: 'Motoko.jpg',
+    id: 'dev-0',
+    telegramLink: 'https://t.me/anastasiia_nih',
+    github: 'https://github.com/matoka02',
+    email: 'a.m.nigomedzyanova@gmail.com',
+    linkedIn: 'www.linkedin.com/in/tessa02/',
+    motto: 'Do. Or do not. There is no try.',
+  },
+];
